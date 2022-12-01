b441b054986db7bdc43dd1701ed251723921873b
Ted Statham <tstatham@local.sequiter.com>
2022-12-01 08:13:04 -0700

    Day 1 completed

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -11,28 +11,61 @@ namespace Day01
         static void Main(string[] args)
         {
             Console.WriteLine("Advent of Code 2022: Day 1");
-            var elfCaloriesRaw = File.ReadLines(@"\.ElfCalories-test.txt").ToList();
-            //var elfCaloriesRaw = File.ReadLines(@"\.ElfCalories-full.txt").ToList();
+            //var elfCaloriesRaw = File.ReadLines(@".\ElfCalories-test.txt").ToList();
+			var elfCaloriesRaw = File.ReadLines(@".\ElfCalories-full.txt").ToList();
 
-            var elvesInParty = elfCaloriesRaw.Where(e => string.IsNullOrEmpty(e)).ToList().Count + 1
+			var elvesInParty = elfCaloriesRaw.Where(e => string.IsNullOrWhiteSpace(e)).ToList().Count + 1;
 
             Console.WriteLine($"* Elf calorie entries read: {elfCaloriesRaw.Count:N0}");
             Console.WriteLine($"* Elves in party: {elvesInParty:N0}");
 
-            PartA();
-            PartB();
+			var elfCalories = ParseElfCalories(elfCaloriesRaw);
+
+            PartA(elfCalories);
+            PartB(elfCalories);
         }
 
-        private static void PartA()
+		private static List<List<int>> ParseElfCalories(List<string> elfCaloriesRaw)
+		{
+			var elfCalories = new List<List<int>>();
+			var calories = new List<int>();
+
+			foreach (var elfCalorie in elfCaloriesRaw)
+			{
+				if (string.IsNullOrWhiteSpace(elfCalorie))
+				{
+					elfCalories.Add(calories);
+					calories = new();
+					continue;
+				}
+
+				calories.Add(int.Parse(elfCalorie));
+			}
+
+			elfCalories.Add(calories);
+			Console.WriteLine($"* Number of calorie entries created: {elfCalories.Count:N0}");
+			return elfCalories;
+		}
+
+        private static void PartA(List<List<int>> elfCalories)
         {
             Console.WriteLine("\r\n**********");
             Console.WriteLine("* Part A");
+
+			var highestCalories = elfCalories.Max(e => e.Sum());
+
+			Console.WriteLine($"*** The highest amount of calories is {highestCalories:N0}");
         }
 
-        private static void PartB()
+        private static void PartB(List<List<int>> elfCalories)
         {
             Console.WriteLine("\r\n**********");
             Console.WriteLine("* Part B");
+
+			var sortedCalories = elfCalories.Select(e => e.Sum()).OrderDescending().Take(3).ToList();
+
+			Console.WriteLine($"* Top 3 highest calories are: {string.Join(" / ", sortedCalories)}");
+			Console.WriteLine($"*** Sum of top 3 highest calories are: {sortedCalories.Sum():N0}");
         }
     }
 }
