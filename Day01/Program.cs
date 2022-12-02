d8bc9f4ce2b442a0433d69c70ebf0c98fa8235f4
Ted Statham <65921212+TedStathamHome@users.noreply.github.com>
2022-12-01 21:24:34 -0700

    Fixed pathing for Linux

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -11,8 +11,8 @@ namespace Day01
         static void Main(string[] args)
         {
             Console.WriteLine("Advent of Code 2022: Day 1");
-            //var elfCaloriesRaw = File.ReadLines(@".\ElfCalories-test.txt").ToList();
-			var elfCaloriesRaw = File.ReadLines(@".\ElfCalories-full.txt").ToList();
+            //var elfCaloriesRaw = File.ReadLines(@"./ElfCalories-test.txt").ToList();
+			var elfCaloriesRaw = File.ReadLines(@"./ElfCalories-full.txt").ToList();
 
 			var elvesInParty = elfCaloriesRaw.Where(e => string.IsNullOrWhiteSpace(e)).ToList().Count + 1;
 
